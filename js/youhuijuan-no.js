b1d62cb780225bde8b3977a40ac430a6bfb1501f
yaoweida <799761810@qq.com>
2018-09-28 17:08:19 +0800

    t

--- js/youhuijuan-no.js
+++
@@ -1,9 +0,0 @@
-window.onload=function(){
-    let list=$(".list");
-    let card=$(".card");
-    list.click(function(){
-        let i=$(this).index();
-        list.removeClass("active").eq(i).addClass("active");
-        card.fadeOut("slow").eq(i).fadeIn("slow");
-    })
-}
